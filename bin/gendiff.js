73649489789d153991a07f9a51e7060bfd39c6cc
Александра <varyandis@yandex.ru>
2025-08-01 10:33:02 +0300

    fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,12 @@
 
 import { program } from 'commander';
 
+const command = () => {};
+
 program
+  .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
