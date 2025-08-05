44adf8ce75c7d069a2c8ebf8522be42f177ba4c8
Александра <varyandis@yandex.ru>
2025-08-05 17:32:11 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { formatStylish } from './formatStylish.js'
 export default function gendiff(filepath1, filepath2) {
   const obj1 = parseFile(filepath1)
   const obj2 = parseFile(filepath2)
-  const objTreeDiff = buildDiff(obj1, obj2)
-  const diff = formatStylish(objTreeDiff)
+  const diffTree = buildDiff(obj1, obj2)
+  const formattedDiff = formatStylish(diffTree)
 
-  return diff
+  return formattedDiff
 }
