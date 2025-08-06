dc17b5890eccdf7d4a58249137c010b557599baa
Александра <varyandis@yandex.ru>
2025-08-06 11:53:17 +0300

    fix buildDiff

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,3 +1,7 @@
+const isObject = (value) => {
+  return typeof value === 'object' && value !== null && !Array.isArray(value)
+}
+
 export const buildDiff = (obj1, obj2) => {
   const keys = [...Object.keys(obj1), ...Object.keys(obj2)]
   const uniqueSortedKeys = keys.filter((key, index) => keys.indexOf(key) === index).sort((a, b) => a.localeCompare(b))
@@ -6,7 +10,12 @@ export const buildDiff = (obj1, obj2) => {
     const oldValue = obj1[key]
     const newValue = obj2[key]
 
-    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+    const valueIsObject = isObject(oldValue) && isObject(newValue)
+
+    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key) && valueIsObject) {
+      return { key, type: 'nested', children: buildDiff(oldValue, newValue) }
+    }
+    else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
       return { key, type: 'removed', value: oldValue }
     }
     else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
